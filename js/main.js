cfa23d532efbbd26ffebdc25fc3f809758486ea4
kibts <124083041+kibts@users.noreply.github.com>
2023-02-02 17:23:32 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var campaignID = "5WYGEOnaJBw6L3be";
+var clientID = "";
+var product = 2151;
+var price = 49;
+var region = 1801;
+
+!function(e,t,r){function n(){for(;d[0]&&"loaded"==d[0][f];)c=d.shift(),c[o]=!i.parentNode.insertBefore(c,i)}for(var s,a,c,d=[],i=e.scripts[0],o="onreadystatechange",f="readyState";s=r.shift();)a=e.createElement(t),"async"in i?(a.async=!1,e.head.appendChild(a)):i[f]?(d.push(a),a[o]=n):e.write("<"+t+' src="'+s+'" defer></'+t+">"),a.src=s}(document,"script",[
+  "https://api.leadwyz.com/script_v4",
+  "https://api.leadwyz.com/js/order.js"
+]);
